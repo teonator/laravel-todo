eb6d2cc1f0b9c31e8fbb3de6ad06149a7d445589
Teon Ooi <teon@protonmail.com>
2023-08-14 23:16:30 +0800

    Change status column name to done.

--- database/migrations/1970_12_31_000000_create_task_table.php
+++ database/migrations/1970_12_31_000000_create_task_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('label');
-            $table->boolean('status')->default(false);
+            $table->boolean('done')->default(false);
         });
     }
 
